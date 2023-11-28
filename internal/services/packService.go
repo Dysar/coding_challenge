e24de7be0a1c3e6ffafb88e7e077cd30c7271f9c
Dysar <david0zin3@gmail.com>
2023-11-28 10:34:44 +0200

    simplified packing algorithm code a bit

--- internal/services/packService.go
+++ internal/services/packService.go
@@ -42,7 +42,6 @@ func (s *PackServiceImpl) CalculatePacks(orderQuantity int) ([]model.PackDetails
 	// Calculate the number of packs needed for each pack size
 	for i, packSize := range s.packSizes {
 		packsCount := remainingQuantity / packSize
-		logrus.Infof("remainingQuantity: %d, packsCount: %d, packSize: %d, i: %d", remainingQuantity, packsCount, packSize, i)
 		if packsCount > 0 {
 			logrus.Infof("adding a pack size %d, count: %d", packSize, packsCount)
 			packMap[packSize] = packsCount
@@ -57,27 +56,19 @@ func (s *PackServiceImpl) CalculatePacks(orderQuantity int) ([]model.PackDetails
 		// if the remainingQuantity is less than the smallest pack try to add the whole quantity to a larger box (if there are larger boxes)
 		// but only if the box is not too large. Let's assume that the first half of boxes are too large
 		if remainingQuantity < smallestPack &&
-			i > len(s.packSizes)/2 && i != lastIndex && packedQuantity != 0 && packsCount != 0 {
+			i > len(s.packSizes)/2 && packedQuantity != 0 && packsCount != 0 {
 			logrus.Infof("removing a pack size %d, count: %d", packSize, 1)
 			packMap[packSize] -= 1
 			logrus.Infof("adding a larger pack size %d, count: %d", s.packSizes[i-1], 1)
-			packMap[s.packSizes[i-1]] = 1
+			packMap[s.packSizes[i-1]] += 1
 			remainingQuantity = 0
 		}
 
 		//if there is any remaining quantity when all the pack sizes have been checked
-		if i == lastIndex {
-			if packsCount != 0 {
-				//if any smallest (250) packs have been filled, we need to upgrade one 250 pack to a 500 pack
-				logrus.Infof("removing a pack size %d, count: %d", packSize, 1)
-				packMap[packSize] -= 1
-				logrus.Infof("adding a pack size %d, count: %d", s.packSizes[i-1], 1)
-				packMap[s.packSizes[i-1]] += 1
-			} else {
-				//fill one smallest pack (250)
-				logrus.Infof("adding one smallest pack size %d, count: %d", packSize, 1)
-				packMap[packSize] += 1
-			}
+		if i == lastIndex && packsCount == 0 {
+			//fill one smallest pack (250)
+			logrus.Infof("adding one smallest pack size %d, count: %d", packSize, 1)
+			packMap[packSize] += 1
 			remainingQuantity = 0
 		}
 	}
